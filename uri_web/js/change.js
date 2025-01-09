ba4c0c1e4fff38af52cf7bb14a81b771274c1d5a
sarmenti7 <84134786+sarmenti7@users.noreply.github.com>
2025-01-09 20:04:18 +0000

    Lab 2 Exercise 1 Starter Code

---
+++ uri_web/js/change.js
@@ -0,0 +1,28 @@
+function changePic() 
+{
+
+  /* write code to change the picture to the one linked at:
+  
+    - https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Rhode_Island_Rams_logo.svg/1200px-Rhode_Island_Rams_logo.svg.png
+    
+	- change the width of the image
+  */
+  
+}
+
+
+function changeTags() 
+{
+
+  /* write code to change the color of all paragraphs to one of your choosing
+  */
+  
+}
+
+
+function removeParagraph() 
+{
+	/* write code to use a confirmation box to confirm that the  user wants to remove the paragraph where this button is located. If the user confirms, then remove this paragraph.
+  */
+  
+}
